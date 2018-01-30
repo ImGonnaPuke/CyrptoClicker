835d543bd7275a1209af28185316fb918b2160cd
ImGonnaPuke <ahorle@oakland.edu>
2018-01-29 23:04:29 -0500

    Added achievements and upgrades page

--- app/src/main/java/com/cryptoclicker/classified/cryptoclicker/MainActivity.java
+++ app/src/main/java/com/cryptoclicker/classified/cryptoclicker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cryptoclicker.classified.cryptoclicker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,10 +14,40 @@ public class MainActivity extends AppCompatActivity {
     TextView showValue;
     int counter = 0;
 
+    public Button Up;
+
+    public void uGrades(){
+        Up =  findViewById(R.id.upgrades);
+        Up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent why = new Intent(MainActivity.this, Upgrades.class);
+                startActivity(why);
+            }
+        });
+    }
+
+    public Button Ach;
+
+    public void Achievements(){
+        Ach =  findViewById(R.id.achi);
+        Ach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent why1 = new Intent(MainActivity.this, Achievements.class);
+                startActivity(why1);
+            }
+        });
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        uGrades();
+        Achievements();
 
         showValue = (TextView) findViewById(R.id.Counter);
     }
@@ -31,12 +62,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"its magic", Toast.LENGTH_SHORT).show();
                 int clicks =0;
                 clicks = clicks+1;
-
             }
-
-
-
-
         });
         */
 

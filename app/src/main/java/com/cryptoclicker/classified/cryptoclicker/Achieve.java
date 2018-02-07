5ba119f1a1144735b10d9913afac8a8379afa6cb
Miguel Rullan <marull1997@gmail.com>
2018-02-07 06:02:44 -0500

    Initial Achievement

---
+++ app/src/main/java/com/cryptoclicker/classified/cryptoclicker/Achieve.java
@@ -0,0 +1,52 @@
+package com.cryptoclicker.classified.cryptoclicker;
+
+/**
+ * Created by Miguel on 1/31/2018.
+ */
+
+public class Achieve implements AchievementsPage {
+    private int cryCount;
+    private int unlockCount;
+    private boolean needsCount;
+    private boolean hasUpgrade;
+    private int hasUpgrades;
+    private boolean unlock;
+    private int effect;
+    private int min;
+    private int max;
+    private String tag;
+
+    public Achieve(int unlockCount, int effect, String tag) {
+        this.unlockCount = unlockCount;
+        this.effect = effect;
+        this.unlock = false;
+        this.needsCount = true;
+        this.min = 0;
+        this.max = unlockCount;
+        this.tag = tag;
+    }
+
+    public Achieve(int effect, String tag) {
+        this.min = 0;
+        this.max = 1;
+        this.effect = effect;
+        this.tag = tag;
+    }
+
+    public void hasUp()
+    {
+        this.hasUpgrade = true;
+    }
+
+    public void unlocked() {
+        this.unlock = true;
+    }
+
+    public void checkCount() {
+        if (needsCount == true) {
+            if (cryCount >= unlockCount) {
+                this.unlocked();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/cryptoclicker/classified/cryptoclicker/AchievementsPage.java
@@ -0,0 +1,9 @@
+package com.cryptoclicker.classified.cryptoclicker;
+
+/**
+ * Created by Miguel on 2/7/2018.
+ */
+
+public interface AchievementsPage {
+
+}

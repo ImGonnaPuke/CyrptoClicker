c644257bc02dadb962b39afe44dc0a851d22026e
Miguel Rullan <marull1997@gmail.com>
2018-02-07 05:37:37 -0500

    Initial Achievement

--- app/src/main/java/com/cryptoclicker/classified/cryptoclicker/Achievements.java
+++ app/src/main/java/com/cryptoclicker/classified/cryptoclicker/Achievements.java
@@ -5,9 +5,16 @@ import android.os.Bundle;
 
 public class Achievements extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievements);
     }
+    Achieve one = new Achieve(10, 0, "Collected 10 coins");
+    Achieve two = new Achieve(100, 0, "Collected 100 coins");
+    Achieve three = new Achieve(1000, 0, "Collected 1000 coins");
+
+    Achieve upOne = new Achieve(0, "Bought your first upgrade");
+
 }

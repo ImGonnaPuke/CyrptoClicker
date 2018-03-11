888a126accafea700b26d54a9176230705af5ced
ImGonnaPuke <ahorle@oakland.edu>
2018-03-10 20:27:09 -0500

    Random button that adds 25% of currency to your own and a show button for testing

--- app/src/main/java/com/cryptoclicker/classified/cryptoclicker/MainActivity.java
+++ app/src/main/java/com/cryptoclicker/classified/cryptoclicker/MainActivity.java
@@ -2,6 +2,7 @@ package com.cryptoclicker.classified.cryptoclicker;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,13 +15,18 @@ import android.widget.Toast;
 import android.widget.TextView;
 import android.os.Handler;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView showValue;
+
+
     double counter = 0;
     public Button Up;
     long tInterval = 500;
     Upgrades upgrades = new Upgrades();
+    public int rand;
 
     public void uGrades(){
         Up =  findViewById(R.id.upgrades);
@@ -53,8 +59,37 @@ public class MainActivity extends AppCompatActivity {
 
     private Handler handler = new Handler();
 
+    public Button show1;
+    public void Show(){
+        show1 =  findViewById(R.id.show);
+       show1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hide1.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    public Button hide1;
+    public void Hide(){
+        hide1 =  findViewById(R.id.hide);
+        hide1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double temp;
+                temp=counter/4;
+                counter=counter*1.25;
+                showValue.setText(Integer.toString((int) counter)+ " BTC");
+                Toast.makeText(getApplicationContext(),"GAINED "+(int)temp+" COINS", Toast.LENGTH_SHORT).show();
+                hide1.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageView backgroundimage = (ImageView) findViewById(R.id.background);
@@ -62,11 +97,26 @@ public class MainActivity extends AppCompatActivity {
         backgroundimage.startAnimation(backgroundrotate);
         uGrades();
         Achievements();
+        Show();
+        Hide();
+
+
+        hide1.setVisibility(View.INVISIBLE);
+
+
 
         showValue = findViewById(R.id.Counter);
 
         handler.postDelayed(runnable, tInterval);
+
+
+
+
+
+
+
     }
+
 
         /*
         Button main1 =  findViewById(R.id.magic);
@@ -86,15 +136,29 @@ public class MainActivity extends AppCompatActivity {
         counter ++;
         playSound();
         showValue.setText(Integer.toString((int) counter)+ " BTC");
+
+       if(random()==0){
+            hide1.setVisibility(View.VISIBLE);
+        }
     }
 
     private Runnable runnable = new Runnable() {
         @Override
         public void run() {
-            increment();
+        //    increment();
+
             handler.postDelayed(this, tInterval);
+
         }
     };
+
+    public int random(){
+
+        //SystemClock.sleep(5000);
+        Random r = new Random();
+        int i1 = r.nextInt(10 )  ;
+        return i1;
+    }
 
     private void increment(){
         counter += upgrades.cycle() * ((double) tInterval/1000);
